0f02ea101cc515b636f0628e4fa4e0a0c56180d8
Priyanka-Nade2000 <nadeps17@gmail.com>
2023-10-26 19:02:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Detail from './components/detail_filling_page/detail';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+
+import Aboutus from './components/about_us_page/aboutus';
+
+import Previewpage from './components/preview_page/priview';
+import Mytemplates from './components/homepage/my_templates';
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter><Routes>
+      <Route path='/' element={<Mytemplates />}></Route>
+      <Route path='det' element={<Detail/>}></Route>
+      <Route path="prev" element={<Previewpage/>}></Route>
+    
+      <Route path='/AboutUs'  element={<Aboutus/>}></Route>
+      
+    </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/state/action/index.js
@@ -0,0 +1,3 @@
+import updateinfo from "./action";
+import { T1,T2, T3, T4} from "./sel_Template";
+export  {updateinfo,T1,T2,T3,T4};
